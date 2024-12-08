cf3af710d4eb1ad16d7b87b39ace60541c7e248f
audynareyda <audynareyda@gmail.com>
2024-12-08 10:40:59 +0700

    q

--- lasol/vite.config.js
+++ lasol/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Ganti `lasol` dengan nama repositori GitHub kamu
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/lasol/", // Tambahkan ini agar GitHub Pages dapat menemukan resource
+});
